a7b3d0f799eabf510fa6adebfdb468a8bde6c9c0
brolfson <bennett.rolfson@ucas-edu.net>
2026-03-04 04:19:27 +0000

    Added the games and hashing

--- src/password.py
+++ src/password.py
@@ -2,11 +2,100 @@ from helper import *
 import bcrypt
 
 def pass_encoder(username, password):
-    password = b"{password}"
-    username = bcrypt.gensalt()
-    return bcrypt.hashpw(password, username)
+    password_bytes = password.encode('utf-8')
+    salt = bcrypt.gensalt()
+    return bcrypt.hashpw(password_bytes, salt)
 
-print(pass_encoder("BobertThe4th", "3gG4s"))
+def pass_checker(password, hashed):
+    password_bytes = password.encode('utf-8')
+    return bcrypt.checkpw(password_bytes, hashed)
 
-def pass_checker():
-    
+def password():
+    passwords = ["1234", "password", "qwerty", "123456", "123456789", "123467890", "0987654321", "111111", "p@ssw0rd", "123123", "p@$$w0rd", "5678", "abcdefghijklmnopqrstuvwxyz", "aBcDeFgHiJkLmNoPqRsTuVwXyZ", "AbCdEfGhIjKlMnOpQrStUvWxYz", "ABCDEFGHIJKLMNOPQRSTUVWXYZ", "qwertyuiop", "asdfghjkl", "asdfghjkl;'", "zxcvbnm,./", "/.,mnbvcxz", "';lkjhgfdsa", "lkjhgfdsa", "poiuytrewq"]
+    #loop
+    while True:
+        #reset the strength to 0
+        strength = 0
+        #reset all the variables to false
+        up = False
+        low = False
+        num = False
+        #Ask the user for the password
+        password = input("Please write your prospective password. ")
+        #if it is longer than 7 letters
+        if len(password) > 7:
+            #add 1 to the password strength
+            strength += 1
+        #otherwise
+        else:
+            #say that they need to make their password longer
+            print("You need to make your password longer. ")
+        #looping through every single letter
+        for letter in password:
+            #check if that letter is uppercase
+            if letter.isupper():
+                #make it so that the information that it is uppercase is true
+                up = True
+            #if it is true
+        if up:
+                #add 1 to the password strength
+            strength += 1
+            #otherwise
+        else:
+                #say that they have to add an uppercase letter
+            print("You need to add an uppercase letter. ")
+        #looping through every single letter
+        for letter in password:
+        #check if that letter is lowercase
+            if letter.islower():
+                #make it so that the information that it is lowercase is true
+                low = True
+        #if it is true
+        if low:
+            #add 1 to the password strength
+            strength += 1
+        #otherwise
+        else:
+            #say that they have to add an lowercase letter
+            print("You need to add a lowercase letter. ")
+        #check if the string is not alpha numeric (isalnum)
+        if not password.isalnum() and password:
+            #add 1 to the password strength
+            strength += 1
+        #otherwise
+        else:
+            #say that they need to add a special character
+            print("You need to add a special character. ")
+        #looping through every single letter
+        for letter in password:
+            #check if that letter is numeric
+            if letter.isdigit():
+                #if it is, make it so that the information that there is a number is true
+                num = True
+        #if it is true
+        if num:
+            #add 1 to the password strength
+            strength += 1
+        #otherwise
+        else:
+            #say that they have to add an number
+            print("You need to add a number. ")
+        #display the password strength
+        print(f"Your password strength is {strength}/5. ")
+        if strength == 5:
+            print("That is very strong. Good job! ")
+        elif strength == 4:
+            print("This is strong but you can do better. ")
+        elif strength == 3:
+            print("Your password is moderate. You have room to improve. ")
+        elif strength == 1 or strength == 2:
+            print("Your password is weak. Do better. ")
+        elif strength == 0:
+            print("You and your password is a failure. ")
+        else:
+            print("You found the easter egg! Don't use it again. ")
+        if password in passwords:
+            print("That is so boring and typical. Why did you choose this password? Get more creative. I don't really like that you did this. ")
+        check = input("Are you satisfied with your password? ").strip().lower()
+        if check == "yes":
+            return password
