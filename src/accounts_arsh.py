7a8b45d67d5e354d10b5894ef065ff3a2abe13e8
BdzUcas <bracken.zabriskie@ucas-edu.net>
2026-03-06 09:33:07 -0700

    bugfixes

--- src/accounts_arsh.py
+++ src/accounts_arsh.py
@@ -9,7 +9,7 @@ accounts = csv_to_dictionary("docs/accounts.csv")
 def check_usernames(search_username):
     username_header = "username"
 
-    with open(target_file_path, mode='r', newline='') as file:
+    with open("docs/accounts.csv", mode='r', newline='') as file:
         reader = csv.DictReader(file)
 
         for row in reader:
@@ -23,7 +23,7 @@ def login():
 
     while typing_username:
         username = input("\nEnter Username: ").strip()
-        username_exist = check_usernames(username)
+        username_exist, user = check_usernames(username)
 
         if username_exist == False:
             while True:
@@ -48,3 +48,4 @@ def login():
                 print("True")
             elif not password_match:
                 print("False")
+    return username
